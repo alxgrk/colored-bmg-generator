5fbe5772c07284df6cc3b0738f3fbf488f44675b
Alexander Girke <alexgirke@gmail.com>
2018-09-21 13:51:30 +0200

    add parser test

--- src/main/java/de/uni/leipzig/parser/Parser.java
+++ src/main/java/de/uni/leipzig/parser/Parser.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Set;
 
-import org.assertj.core.util.Sets;
+import com.google.common.collect.Sets;
 
 import de.uni.leipzig.model.DiGraph;
 import de.uni.leipzig.model.Node;
@@ -14,32 +14,32 @@ import de.uni.leipzig.model.edges.DiEdge;
 
 public class Parser {
 
-	public DiGraph parse(File input) throws IOException {
-		
-		if (!input.exists() || !input.canRead()) {
-			throw new IllegalArgumentException("File not readable!!");
-		}
-		
-		try (BufferedReader reader = new BufferedReader(new FileReader(input));) {
-			
-			Set<Node> nodes = Sets.newHashSet();
-			Set<DiEdge> edges = Sets.newHashSet();
-			
-			reader.lines()
-				.filter(f -> f.startsWith("#"))
-				.map(l -> new BlastGraphLine(l))
-				.forEach(l -> {
-					Node node1 = l.getGene1().asNode();
-					Node node2 = l.getGene2().asNode();
-					nodes.add(node1);
-					nodes.add(node2);
-					
-					edges.add(new DiEdge(node1, node2));
-					edges.add(new DiEdge(node2, node1));
-				});
-			
-			return new DiGraph(nodes, edges);
-		}
-	}
-	
+    public DiGraph parse(File input) throws IOException {
+
+        if (!input.exists() || !input.canRead()) {
+            throw new IllegalArgumentException("File not readable!!");
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(input));) {
+
+            Set<Node> nodes = Sets.newHashSet();
+            Set<DiEdge> edges = Sets.newHashSet();
+
+            reader.lines()
+                    .filter(f -> f.startsWith("#"))
+                    .map(l -> new BlastGraphLine(l))
+                    .forEach(l -> {
+                        Node node1 = l.getGene1().asNode();
+                        Node node2 = l.getGene2().asNode();
+                        nodes.add(node1);
+                        nodes.add(node2);
+
+                        edges.add(new DiEdge(node1, node2));
+                        edges.add(new DiEdge(node2, node1));
+                    });
+
+            return new DiGraph(nodes, edges);
+        }
+    }
+
 }
--- src/test/java/de/uni/leipzig/parser/ParserTest.java
+++ src/test/java/de/uni/leipzig/parser/ParserTest.java
@@ -1,5 +1,24 @@
 package de.uni.leipzig.parser;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import de.uni.leipzig.model.DiGraph;
+
 public class ParserTest {
+
+    @Test
+    public void testParser() throws Exception {
+
+        File input = new File("myproject.blast-graph");
+
+        Parser uut = new Parser();
+        DiGraph graph = uut.parse(input);
+
+        assertThat(graph).isNotNull();
+    }
 
 }
