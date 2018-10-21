e26e9bfb50c820ee3ce9579927be7d8c1a8dd8cd
Alexander Girke <alexgirke@gmail.com>
2018-10-21 14:31:56 +0200

    TripleFromTree

--- src/main/java/de/uni/leipzig/conversion/TripleFromTree.java
+++ src/main/java/de/uni/leipzig/conversion/TripleFromTree.java
@@ -3,9 +3,15 @@ package de.uni.leipzig.conversion;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.*;
+
+import de.uni.leipzig.Util;
 import de.uni.leipzig.model.*;
+import de.uni.leipzig.model.edges.Edge;
 
 public class TripleFromTree {
+
+    private Multimap<Node, List<Node>> parentLeafSetsPerLeaf = LinkedListMultimap.create();
 
     public Set<Triple> extractOf(Map<Pair<Color>, Tree> stTrees) {
         return stTrees.values()
@@ -16,7 +22,74 @@ public class TripleFromTree {
 
     public Set<Triple> treeToTriples(Tree tree) {
 
-        return null;
+        List<Node> allLeafs = tree.getLeafs();
+        allLeafs.forEach(n -> parentLeafSetsPerLeaf.put(n, allLeafs));
+        fillMultiMap(tree);
+
+        Set<Triple> triples = Sets.newHashSet();
+
+        parentLeafSetsPerLeaf.asMap().forEach((a, va) -> {
+
+            parentLeafSetsPerLeaf.asMap().forEach((b, vb) -> {
+                if (a.equals(b))
+                    return;
+
+                if (a.getColor().equals(b.getColor()))
+                    return;
+
+                Optional<List<Node>> lcaOpt = findLca(va, vb);
+
+                parentLeafSetsPerLeaf.asMap().forEach((c, vc) -> {
+                    if (a.equals(c) || b.equals(c))
+                        return;
+
+                    // TODO is this correct?
+                    // currently excludes all same-colored / all-distinct-colored triples
+                    if ((a.getColor().equals(c.getColor()) && b.getColor().equals(c.getColor()))
+                            || (!a.getColor().equals(c.getColor()) && !b.getColor()
+                                    .equals(c.getColor())))
+                        return;
+
+                    lcaOpt.ifPresent(lca -> {
+                        for (List<Node> cParent : vc) {
+                            if (Util.properSubset(lca, cParent))
+                                triples.add(new DefaultTriple(new Edge(a, b), c));
+                        }
+                    });
+                });
+            });
+
+        });
+
+        parentLeafSetsPerLeaf.clear();
+
+        return triples;
     }
 
+    private void fillMultiMap(Tree tree) {
+
+        for (Tree subTree : tree.getSubTrees()) {
+            List<Node> leafs = subTree.getLeafs();
+
+            // skip leaf trees
+            if (leafs.size() > 1) {
+                for (Node node : leafs) {
+                    parentLeafSetsPerLeaf.put(node, leafs);
+                }
+            }
+
+            fillMultiMap(subTree);
+        }
+    }
+
+    private Optional<List<Node>> findLca(Collection<List<Node>> va, Collection<List<Node>> vb) {
+        Set<List<Node>> parentLeafSetsA = Sets.newHashSet(va);
+        Set<List<Node>> parentLeafSetsB = Sets.newHashSet(vb);
+
+        Optional<List<Node>> lca = Sets.intersection(parentLeafSetsA, parentLeafSetsB)
+                .stream()
+                .min((l1, l2) -> Integer.compare(l1.size(), l2.size()));
+
+        return lca;
+    }
 }
---
+++ src/test/java/de/uni/leipzig/conversion/TripleFromTreeTest.java
@@ -0,0 +1,79 @@
+package de.uni.leipzig.conversion;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.*;
+
+import org.junit.*;
+
+import com.google.common.collect.*;
+
+import de.uni.leipzig.Util;
+import de.uni.leipzig.model.*;
+import de.uni.leipzig.uncolored.AhoBuild;
+
+public class TripleFromTreeTest {
+
+    // Example tree taken from paper
+    // @formatter:off
+    //          _____0_____
+    //         /     | \   \
+    //        /      |  \   \
+    //       /       |   \   \
+    //      /        |   (9) (10)
+    //    /|\       /|\
+    //   / | \     / | \
+    // (3) | [4] (7) | (8)
+    //     |         |
+    //    / \       / \
+    //  (1) [2]   (5) [6]
+
+    // Nodes: [0-0111, 1-0112, 0-012, 1-013, 0-0211, 1-0212, 0-022, 0-023,
+    // 0-03, 0-04]
+    Node one = Node.of(1, Lists.newArrayList(0, 1, 1, 1));
+    Node two = Node.of(0, Lists.newArrayList(0, 1, 1, 2));
+    Node three = Node.of(1, Lists.newArrayList(0, 1, 2));
+    Node four = Node.of(0, Lists.newArrayList(0, 1, 3));
+    Node five = Node.of(0, Lists.newArrayList(0, 2, 1, 1));
+    Node six = Node.of(0, Lists.newArrayList(0, 2, 1, 2));
+    Node seven = Node.of(1, Lists.newArrayList(0, 2, 2));
+    Node eight = Node.of(1, Lists.newArrayList(0, 2, 3));
+    Node nine = Node.of(0, Lists.newArrayList(0, 3));
+    Node ten = Node.of(0, Lists.newArrayList(0, 4));
+    
+    private Tree root;
+    private HashSet<Node> allLeafs;
+    
+    // @formatter:on
+
+    @Before
+    public void setUp() throws Exception {
+        Tree tree12 = Util.nodeSetToLeafTree(Sets.newHashSet(one, two));
+        Tree tree56 = Util.nodeSetToLeafTree(Sets.newHashSet(five, six));
+
+        Tree tree1234 = Util.nodeSetToLeafTree(Sets.newHashSet(one, two, three, four));
+        tree1234.addSubTree(tree12);
+        Tree tree5678 = Util.nodeSetToLeafTree(Sets.newHashSet(five, six, seven, eight));
+        tree5678.addSubTree(tree56);
+
+        allLeafs = Sets.newHashSet(one, two, three, four, five, six, seven,
+                eight, nine, ten);
+        root = Util.nodeSetToLeafTree(allLeafs);
+        root.addSubTree(tree1234);
+        root.addSubTree(tree5678);
+    }
+
+    @Test
+    public void testRoot() throws Exception {
+
+        TripleFromTree uut = new TripleFromTree();
+        Set<Triple> actual = uut.treeToTriples(root);
+
+        // FIXME
+        AhoBuild ahoBuild = new AhoBuild();
+        Tree result = ahoBuild.build(actual, allLeafs);
+
+        assertThat(actual).isNotNull();
+    }
+
+}
